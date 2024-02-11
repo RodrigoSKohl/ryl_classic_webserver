56eae4b7e750880cba2beb247f1daf34a5cc85e8
RodrigoSKohl <rodrigokohl1@yahoo.com.br>
2024-02-11 02:34:47 -0300

    muitas funcionalidades adicionadas

--- backend/api.js
+++ backend/api.js
@@ -12,19 +12,28 @@ const https = require('https');
 const fs = require('fs');
 //lib para ler caminhos
 const path = require('path');
-//lib para gravar logs
-const morgan = require('morgan');
+//lib dos cookies
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
 //variaveis de ambiente(mudar em .env )
 require('dotenv').config();
 
-
+//Setup Express
 const app = express();
+app.use(express.json()); 
+app.use(express.static(path.join(__dirname, 'public')));
+// Set up view engine
+app.set('view engine', 'ejs');
 const localport = process.env.LOCAL_PORT
 const port = process.env.HTTPS_PORT;
 const localhost = process.env.LOCAL_HOST;
+//Variaveis do SK do Captcha e Site
 const hcaptchaSecretKey = process.env.CAPTCHA_SECRET_KEY;
 const hcaptchasite = process.env.HCAPTCHA_SITE;
+// Origens do CORS
 const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+// Nome da tabela a partir da variável de ambiente
+const dbTable = process.env.DB_TABLE;
 
 
 // Configurações do SSL
@@ -46,113 +55,118 @@ const dbConfig = {
   },
 };
 
-// Nome da tabela a partir da variável de ambiente
-const dbTable = process.env.DB_TABLE;
-
-// Crie um stream de gravação para salvar os logs em um arquivo
-const logStream = fs.createWriteStream('logs.txt', { flags: 'a' });
-// Middleware Morgan para gerar logs HTTP e direcioná-los para o stream de gravação
-app.use(morgan('combined', { stream: logStream }));
-
-
-// Middleware CORS
-const allOrigins = [...allowedOrigins, ...localhost]; //add outras origens em env caso utilizar, por padrao add locoalhost na array
-const corsOptions = {
-  origin: allOrigins, 
-  methods: 'GET,POST,OPTIONS',
-  allowedHeaders: 'Content-Type',
-  optionsSuccessStatus: 204, // alguns navegadores 204 não interpretam como erro
-};
-
-app.use(cors(corsOptions)); 
-
-// Middleware para interpretar JSON
-app.use(express.json());
-
 // Criar uma única instância de pool de conexão para ser reutilizada
 const pool = new mssql.ConnectionPool(dbConfig);
 
-app.get('/', (_, res) => {
-  res.send('API ONLINE');
+
+// Middlewares csrf
+app.use(cookieParser()); // Add cookie parser middleware
+const csrfProtect = csrf({ cookie: true }); //CSRF protection confs
+app.use(express.urlencoded({ extended: false })); // 'application/x-www-form-urlencoded'
+
+
+// Middleware CORS
+const corsOptions = {
+  origin: allowedOrigins, 
+  methods: 'GET,POST,OPTIONS',
+  allowedHeaders: 'Content-Type',
+  credentials: true,
+  optionsSuccessStatus: 204, // alguns navegadores 204 não interpretam como erro
+};
+app.use(cors(corsOptions)); 
+
+
+app.get('/register', csrfProtect, function(req, res) {
+  // Generate a token and send it to the view
+  res.render('register', { csrfToken: req.csrfToken() });
 });
 
-
 // Rota para criar um novo usuário
-app.post('/registrar', async (req, res) => {
+
+app.post('/api/register', csrfProtect, async (req, res) => {
   let connection;
 
   try {
-    const { username, senha, confirmarSenha, email, hcaptchaToken } = req.body;
-
-    // Conectar ao banco de dados utilizando a instância única de pool
-    connection = await pool.connect();
-
-    // Validar o formato do username
-    if (!validator.isAlphanumeric(username)) {
-      return res.status(400).send('Formato inválido para o username.');
-    }
-
-    // Verificar se o username já existe no banco de dados
-    const existingUser = await checkExistingUser(username, connection);
-    if (existingUser) {
-      return res.status(400).send('Username já está em uso.');
-    }
-
-    // Validar a senha
-    if (senha.length < 6) {
-      return res.status(400).send('A senha deve ter pelo menos 6 caracteres.');
-    }
-
-    // Confirmar se as senhas são iguais
-    if (senha !== confirmarSenha) {
-      return res.status(400).send('As senhas não coincidem.');
-    }
-
-    // Validar o formato do email
-    if (!validator.isEmail(email)) {
-      return res.status(400).send('Formato inválido para o email.');
-    }
-
-    // Verificar se o email já existe no banco de dados
-    const existingEmail = await checkExistingEmail(email, connection);
-    if (existingEmail) {
-      return res.status(400).send('Email já está em uso.');
-    }
+    const { username, senha, confirmarSenha, email, 'h-captcha-response': hcaptchaToken } = req.body;
 
     // Validar o hCaptcha
     const isHcaptchaValid = await validateHcaptcha(hcaptchaToken);
     if (!isHcaptchaValid) {
-      return res.status(400).send('Falha na verificação hCaptcha.');
+      return res.status(400).json({ error: 'Falha na verificação hCaptcha.' });
     }
 
-// Inserir usuário, senha e email na tabela usando consulta parametrizada
-const result = await connection.request()
-  .input('username', mssql.VarChar(12), username)
-  .input('senha', mssql.VarChar(30), senha)
-  .input('email', mssql.VarChar(50), email)
-  .query(`
-    INSERT INTO ${dbTable} (account, passwd, email)
-    OUTPUT Inserted.account, Inserted.email
-    VALUES (@username, @senha, @email)
-  `);
 
-// Imprimir os resultados
-if (result.rowsAffected && result.rowsAffected[0] === 1) {
-  const createdUser = result.recordset[0];
+    // Conectar ao banco de dados utilizando a instância única de pool
+    connection = await pool.connect();
 
-  // Logar no console
-  console.log('Usuário criado com sucesso:');
-  console.log('Username:', createdUser.account);
-  console.log('Email:', createdUser.email);
+    const errors = [];
 
-  res.status(201).send(`Usuário registrado com sucesso!\n\nUsername: ${createdUser.account}\nEmail: ${createdUser.email}`);
-} else {
-  console.error('Erro ao criar usuário: Nenhuma linha afetada pela inserção.');
-  res.status(500).send('Erro interno do servidor.');
-}
+    // Validar o formato do username
+    if (!validator.isAlphanumeric(username)) {
+      errors.push('Formato inválido para o username.');
+    }
+    
+    // Verificar se o username já existe no banco de dados
+    const existingUser = await checkExistingUser(username, connection);
+    if (existingUser) {
+       errors.push('Username já está em uso.');
+    }
+    
+    // Validar a senha
+    if (senha.length < 6) {
+       errors.push('A senha deve ter pelo menos 6 caracteres.');
+    }
+    
+    // Confirmar se as senhas são iguais
+    if (senha !== confirmarSenha) {
+       errors.push('As senhas não coincidem.');
+    }
+    
+    // Validar o formato do email
+    if (!validator.isEmail(email)) {
+       errors.push('Formato inválido para o email.');
+    }
+    
+    // Verificar se o email já existe no banco de dados
+    const existingEmail = await checkExistingEmail(email, connection);
+    if (existingEmail) {
+       errors.push('Email já está em uso.');
+    }
+    
+    // Verificar se há erros e retornar o status adequado
+    if (errors.length > 0) {
+       return res.status(400).json({ errors });
+    }
+
+
+    // Inserir usuário, senha e email na tabela usando consulta parametrizada
+    const result = await connection.request()
+      .input('username', mssql.VarChar(12), username)
+      .input('senha', mssql.VarChar(30), senha)
+      .input('email', mssql.VarChar(50), email)
+      .query(`
+        INSERT INTO ${dbTable} (account, passwd, email)
+        OUTPUT Inserted.account, Inserted.email
+        VALUES (@username, @senha, @email)
+      `);
+
+    // Imprimir os resultados
+    if (result.rowsAffected && result.rowsAffected[0] === 1) {
+      const createdUser = result.recordset[0];
+
+      // Logar no console
+      console.log('Usuário criado com sucesso:');
+      console.log('Username:', createdUser.account);
+      console.log('Email:', createdUser.email);
+
+      res.status(201).json({ success: 'Usuário registrado com sucesso!', user: createdUser });
+    } else {
+      console.error('Erro ao criar usuário: Nenhuma linha afetada pela inserção.');
+      res.status(500).json({ error: 'Erro interno do servidor.' });
+    }
   } catch (err) {
     console.error('Erro ao criar usuário:', err.message);
-    res.status(500).send('Erro interno do servidor.');
+    res.status(500).json({ error: 'Erro interno do servidor.' });
   } finally {
     // Sempre libere a conexão, mesmo se ocorrer um erro
     if (connection) {
@@ -172,6 +186,7 @@ async function validateHcaptcha(token) {
   });
 
   const data = await response.json();
+  console.log(data);
   return data.success;
 }
 
@@ -207,7 +222,7 @@ const server = https.createServer(sslOptions, app);
 
 // Iniciar o servidor local - para testes
 //Utilze NKGROK OU LOCALTUNNEL para acessar pelo host local publicamente
-app.listen(localport, localhost, () => {
+app.listen(localport,() => {
   console.log(`Servidor rodando localmente em ${localhost}:${localport}`);
 });
 
---
+++ backend/public/scripts/register.js
@@ -0,0 +1,42 @@
+document.getElementById('registroForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const messageContainer = document.getElementById('message-container');
+    const formData = new FormData(this);
+
+    await fetch('/api/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'CSRF-Token': document.querySelector('input[name="_csrf"]').value,
+        },
+        body: JSON.stringify(Object.fromEntries(formData)),
+
+
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            document.getElementById('registroForm').reset();
+            messageContainer.innerHTML = `<p class="success">${data.success}</p>`;
+        } else if (data.error || (data.errors && data.errors.length > 0)) {
+            window.hcaptcha.reset(); 
+            messageContainer.innerHTML = '';
+            
+            if (data.error) {
+                messageContainer.innerHTML += `<p class="error">${data.error}</p>`;
+            }
+        
+            // Exibir cada mensagem de erro
+            if (data.errors) {
+                data.errors.forEach(error => {
+                    messageContainer.innerHTML += `<p class="error">${error}</p>`;
+                });
+            }
+        }
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        messageContainer.innerHTML = '<p class="error">Erro interno do servidor.</p>';
+    });
+});
+
