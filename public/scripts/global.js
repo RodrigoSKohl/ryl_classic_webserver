c0f9fa051f23d25b6742d8d1261d013530d54230
RodrigoSKohl <rodrigokohl1@yahoo.com.br>
2024-03-31 01:34:58 -0300

    update navbar

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -16,3 +16,20 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   });
 });
+
+//Evento scroll solido do navbar
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header'); // Seleciona o header
+    var navbar = document.querySelector('.navbar'); // Seleciona o navbar
+    var scrolled = window.scrollY;
+  
+    // Se o usuário fez scroll, torna o navbar sólido, caso contrário, torna transparente
+    if (scrolled > 0) {
+        header.style.backgroundColor = '#333'; // Define a cor de fundo do navbar
+        navbar.style.borderBottom = "none"; // Aqui está a correção
+    } else {
+        header.style.backgroundColor = 'transparent'; // Torna o navbar transparente
+        navbar.style.borderBottom = "2px ridge #cccccc3f";
+    }
+    header.style.transition = "background-color 0.3s ease"; // Adiciona uma transição suave
+  });
