d1af1ea323b26f313a5d467d2bb525c35402c410
RodrigoSKohl <rodrigokohl1@yahoo.com.br>
2024-07-11 00:07:13 -0300

    removed constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
-// Used for storing constants that are used throughout the application
+// Usado para armazenar constantes que são usadas em todo o projeto.
 module.exports = {
-    downloadURL: 'https://mega.nz/file/ebQ0DB5Q#x436lT7IHOAUUINXnHE8ymKmKq-OBFOTCOHhb7l0Hpw',
-    patchURL: 'https://mega.nz/file/jXJDHRaL#bQna2OboAXjs5Ant2CFiXsyx43_GEj3b0qfB4jaEBTA',
-    discordInvite: 'https://discord.gg/DcXmwsjZTM',
+    downloadURL: '/',
+    patchURL: '/',
+    discordInvite: '/',
 };
